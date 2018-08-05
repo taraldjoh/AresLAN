5eb80b0b2330e360f10c2ef917b3498805888252
taraldjoh <tarald.johansen@outlook.com>
2018-08-05 16:47:18 +0200

    added modal on kjøp buttons

--- index.js
+++ index.js
@@ -2,40 +2,45 @@ window.onload = start;
 
 function start() {
     openModal();
-    animate();
-    document.querySelector(".scroll-effect").addEventListener("click", animate);
-}
-
-function animate() {
-    $('html,body').animate({
-        scrollTop: $(".box-1").offset().top - 25
-    },'slow');
 }
 
 function openModal() {
     // Get the modal
     var modal = document.getElementById('myModal');
 
-    // Get the button that opens the modal
-    var btn = document.getElementById('myBtn');
-
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
 
-    // When the user clicks on the button, open the modal 
-    btn.onclick = function() { 
-        modal.style.display = "block";
-    }
+    // Get the closeBtn that can close the modal
+
+    var btn = document.getElementById('clsBtn');
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
         modal.style.display = "none";
     }
 
+    btn.onclick = function() {
+        modal.className = 'modal animated fadeOutDown';
+            setTimeout(() => {
+                modal.style.display = 'none';
+                modal.className = 'modal animated fadeInDown'
+            }, 750);
+    }
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
         }
     }
+
+    // Modal trigger 
+    const modalTriggers = document.querySelectorAll('.myBtn');
+    
+    modalTriggers.forEach(trigger => {
+        trigger.addEventListener('click', () => {
+            modal.style.display = 'block';
+        });
+    });
 }
+
